4ca460e147d2fe080305dea487166c92ecf1885d
familyboat <84062528+familyboat@users.noreply.github.com>
2023-03-02 11:22:53 +0000

    deprecated: removed fake-chat-server, fake-video-serverand main-site

--- fake-chat-server/deps.ts
+++
@@ -1,3 +0,0 @@
-export { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-export * as uuid from "https://deno.land/std@0.168.0/uuid/mod.ts";
-export * as path from "https://deno.land/std@0.168.0/path/mod.ts";
