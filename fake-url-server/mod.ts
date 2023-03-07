a50457ce48ac10751a52b111c0cc20820734de80
familyboat <familyboat@163.com>
2023-03-07 16:58:23 +0800

    chore: proxy request

--- fake-url-server/mod.ts
+++ fake-url-server/mod.ts
@@ -7,13 +7,18 @@ const listener = Deno.listen({
   port: 8080,
 });
 
+async function fetchUrl(url: string, req: Request) {
+  return await fetch(new Request(url, {...req}))
+}
+
 async function handle(conn: Deno.Conn) {
   for await (const e of Deno.serveHttp(conn)) {
     const url = parseUrl(e.request.url);
     if (!url) {
       return e.respondWith(new Response("You should append url"));
     }
-    const resp = await fetch(url);
+
+    const resp = await fetchUrl(url, e.request);
     return e.respondWith(resp);
   }
 }
