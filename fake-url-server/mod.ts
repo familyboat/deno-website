7a2a9f7a8ef87bdf94fbed2db86956a51c26c1a7
familyboat <2015301110129@whu.edu.cn>
2025-03-28 11:06:27 +0800

    separate sync and async task

--- fake-url-server/mod.ts
+++ fake-url-server/mod.ts
@@ -1,20 +1,23 @@
-import {Hono} from "@hono/hono"
-import { compress } from '@hono/hono/compress'
-import { proxy } from '@hono/hono/proxy'
+import { Hono } from "@hono/hono";
+import { compress } from "@hono/hono/compress";
+import { proxy } from "@hono/hono/proxy";
 
 const app = new Hono();
-app.use(compress())
+app.use(compress());
 app.get("/", (c) => {
   const url = c.req.query("url");
   if (!url) {
-    return Promise.resolve(new Response("You must provide a url", { status: 400 }))
+    const res = c.text("URL is required", 400);
+    res.headers.set("X-URL-Required", "true");
+    return res;
   }
 
-  return proxy(url).then((response) => {
+  return (async () => {
+    const response = await proxy(url);
     response.headers.set("X-Version", "1.0");
     response.headers.set("X-Proxy", "Hono");
     return response;
-  })
-})
+  })();
+});
 
 export default app;
