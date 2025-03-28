55b575b50cd195933e4ea0c82417bc0a9778ebf2
familyboat <2015301110129@whu.edu.cn>
2025-03-28 09:40:32 +0800

    use hono for api server

--- fake-url-server/mod.ts
+++ fake-url-server/mod.ts
@@ -1,36 +1,16 @@
-function parseUrl(url: string) {
-  const u = new URL(url);
-  return u.searchParams.get("url");
-}
+import {Hono} from "@hono/hono"
+import { compress } from '@hono/hono/compress'
+import { proxy } from '@hono/hono/proxy'
 
-async function fetchUrl(url: string, req: Request) {
-  return await fetch(new Request(url, {...req}))
-}
-
-async function handle(conn: Deno.Conn) {
-  for await (const e of Deno.serveHttp(conn)) {
-    const url = parseUrl(e.request.url);
-    if (!url) {
-      return e.respondWith(new Response("You should append url"));
-    }
-
-    const resp = await fetchUrl(url, e.request);
-    return e.respondWith(resp);
+const app = new Hono();
+app.use(compress())
+app.get("/", (c) => {
+  const url = c.req.query("url");
+  if (!url) {
+    return c.text("You should append url");
   }
-}
 
-try {
-  const port = parseInt(Deno.args.at(0) || '') || 8082
+  return proxy(url)
+})
 
-  const listener = Deno.listen({
-    port
-  });
-
-  console.log(`Listening on: http://0.0.0.0:${port}`)
-
-  for await (const conn of listener) {
-    handle(conn);
-  }
-} catch (e) {
-  console.log(e)
-}
+export default app;
