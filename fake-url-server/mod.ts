05b5b79018947fffad1d9030e466bb60229a946f
familyboat <2015301110129@whu.edu.cn>
2025-03-29 11:38:59 +0800

    try handle in-site resource

--- fake-url-server/mod.ts
+++ fake-url-server/mod.ts
@@ -1,6 +1,7 @@
 import { Hono } from "@hono/hono";
 import { compress } from "@hono/hono/compress";
 import { proxy } from "@hono/hono/proxy";
+import {getCookies} from '@std/http/cookie'
 
 const app = new Hono();
 app.use(compress());
@@ -13,11 +14,38 @@ app.get("/", (c) => {
   }
 
   return (async () => {
-    const response = await proxy(url);
-    response.headers.set("X-Version", "1.0");
-    response.headers.set("X-Proxy", "Hono");
-    return response;
+    try {
+      const response = await proxy(url);
+      response.headers.set("X-Version", "1.0");
+      response.headers.set("X-Proxy", "Hono");
+      response.headers.set("Set-Cookie", `proxy-url=${url}`)
+      return response;
+    } catch (_e) {
+      return c.text(`Url is malformed: ${url}`)
+    }
   })();
 });
+
+app.get("/*", (c) => {
+  const cookies = getCookies(c.req.raw.headers);
+  const proxyUrl = cookies['proxy-url'];
+
+  if (!proxyUrl) {
+    return c.text('Can not deal with the request without proxy url cookie');
+  }
+
+  const path = new URL(c.req.url).pathname;
+  try {
+    const origin = new URL(proxyUrl).origin;
+    return (async () => {
+      const realUrl = `${origin}${path}`
+      const response = await proxy(realUrl);
+      response.headers.set('Real-URL', realUrl);
+      return response
+    })()
+  } catch (_e) {
+    return c.text(`Proxy url is malformed: ${proxyUrl}`)
+  }
+})
 
 export default app;
